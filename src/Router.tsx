8fe7e969e3c40a052d368e6c599e47d56726b8a6
soberdeer <antongraywing@gmail.com>
2026-04-07 21:20:24 +0300

    router update

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+  ],
   {
-    path: '/',
-    element: <HomePage />,
-  },
-]);
+    basename: '/chainsaw_org',
+  }
+);
 
 export function Router() {
   return <RouterProvider router={router} />;
